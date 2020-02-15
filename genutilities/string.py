c8ac825d3de9e9fd5cc0a1424ea584b1e1219029
LavaPower <lavapower84@gmail.com>
2020-02-15 18:23:16 +0100

    Add 'random', 'reverse', 'shuffle', 'unique' and some convention's converters un string

--- genutilities/string.py
+++ genutilities/string.py
@@ -1,3 +1,6 @@
+import string
+import random as rand
+
 def remove(text: str, to_remove):
     if isinstance(to_remove, (list, tuple)):
         for i in to_remove:
@@ -7,3 +10,47 @@ def remove(text: str, to_remove):
     else:
         raise TypeError("Second Argument must be a list or a str.")
     return text
+
+def random(n=30, letters = string.ascii_letters + string.digits + string.punctuation + " "):
+    return ''.join(rand.choice(letters) for i in range(n))
+
+def reverse(text: str):
+    return text[::-1]
+
+def shuffle(text: str):
+    return ''.join(rand.sample(text, len(text)))
+
+def unique(text: str):
+    l = []
+    for i in text:
+        if i not in l:
+            l.append(i)
+    return ''.join(l)
+
+def snake_to_pascal(text: str):
+    l = text.split("_")
+    return "".join([i.capitalize() for i in l])
+
+def pascal_to_snake(text: str):
+    l = []
+    for i in text:
+        if i in string.ascii_uppercase:
+            if l != []:
+                l.append("_")
+            l.append(i.lower())
+        else:
+            l.append(i)
+    return "".join(l)
+
+camel_to_snake = pascal_to_snake
+
+def pascal_to_camel(text: str):
+    if len(text):
+        text = text[0].lower() + text[1:]
+    return text
+
+def camel_to_pascal(text: str):
+    return text[0].upper() + text[1:]
+
+def snake_to_camel(text: str):
+    return pascal_to_camel(snake_to_pascal(text))
--- tests/string.py
+++ tests/string.py
@@ -13,3 +13,35 @@ class ModificationTests(unittest.TestCase):
             string.remove(text, 4)
 
         self.assertTrue("Second Argument must be a list or a str." in str(ctx.exception))
+    
+    def test_random(self):
+        r1 = string.random()
+        r2 = string.random(10)
+        r3 = string.random(4, "abc")
+        self.assertEqual(len(r1), 30)
+        self.assertEqual(len(r2), 10)
+        self.assertEqual(len(r3), 4)
+        print("\nRandom String 1 (Default parameters): {}\nRandom String 2 (Len 10): {}\nRandom String 3 (Len 4, letters = 'a', 'b', 'c'): {}".format(r1, r2, r3))
+    
+    def test_reverse(self):
+        self.assertEqual(string.reverse("oui"), "iuo")
+        self.assertEqual(string.reverse("Ceci est à l'endroit"), "tiordne'l à tse iceC")
+    
+    def test_shuffle(self):
+        s1 = string.shuffle("Ceci est un test")
+        for i in s1:
+            self.assertIn(i, "Ceci est un test")
+        self.assertEqual(len(s1), len("Ceci est un test"))
+        print("\nShuffle de 'Ceci est un test': {}".format(s1))
+    
+    def test_unique(self):
+        self.assertEqual(string.unique("oui"), "oui")
+        self.assertEqual(string.unique("Ceci est un test"), "Ceci stun")
+    
+    def test_convertion_between_convention(self):
+        self.assertEqual(string.snake_to_pascal("snake_to_pascal"), "SnakeToPascal")
+        self.assertEqual(string.pascal_to_snake("PascalToSnake"), "pascal_to_snake")
+        self.assertEqual(string.snake_to_camel("snake_to_camel"), "snakeToCamel")
+        self.assertEqual(string.camel_to_snake("camelToSnake"), "camel_to_snake")
+        self.assertEqual(string.camel_to_pascal("camelToPascal"), "CamelToPascal")
+        self.assertEqual(string.pascal_to_camel("PascalToCamel"), "pascalToCamel")
