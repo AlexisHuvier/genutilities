de1a86777efa61eade054de48e659be498d7f8e1
LavaPower <lavapower84@gmail.com>
2020-02-15 18:21:12 +0100

    Remove to_xml in json

--- genutilities/json.py
+++ genutilities/json.py
@@ -56,39 +56,6 @@ class JsonObject:
                     dic[i] = {}
                     dic = dic[i]
 
-    def __to_xml_list(self, root_name, element_name, liste):
-        xml = ["<{}>".format(root_name)]
-        for i in liste:
-            if isinstance(i, dict):
-                for k in self.__to_xml(element_name, element_name, i):
-                    xml.append("   "+k)
-            elif isinstance(i, list):
-                for k in self.__to_xml_list(element_name, element_name, i):
-                    xml.append("   "+k)
-            else:
-                xml.append("   <{0}>{1}</{0}>".format(element_name, i))
-        xml.append("</{}>".format(root_name))
-        return xml
-
-    def __to_xml(self, root_name, element_name, dic):
-        xml = ["<{}>".format(root_name)]
-        for k, v in dic.items():
-            if isinstance(v, dict):
-                for i in self.__to_xml(k, element_name, v):
-                    xml.append("   "+i)
-            elif isinstance(v, (list, tuple)):
-                for i in self.__to_xml_list(k, element_name, v):
-                    xml.append("   "+i)
-            else:
-                xml.append("   <{0}>{1}</{0}>".format(k, str(v)))
-        xml.append("</{}>".format(root_name))
-        return xml
-            
-    def to_xml(self, root_name: str = "root", element_name: str = "element"):
-        xml = ['<?xml version="1.0" encoding="UTF-8"?>']
-        xml.extend(self.__to_xml(root_name, element_name, self.dic))
-        return "\n".join(xml)
-
     def clear(self):
         self.dic = {}
     
--- tests/json.py
+++ tests/json.py
@@ -6,7 +6,7 @@ class JsonObjectTests(unittest.TestCase):
     def setUp(self):
         self.obj = json.JsonObject("tests/test.json", {"config": "default"}, saved_destroy=False)
 
-    def test_obj(self):
+    def test_basic_function(self):
         self.assertEqual(str(self.obj), '{\n    "config": "other"\n}')
         self.assertEqual(self.obj["config"], "other")
         self.obj["joueurs.michel"] = 3
@@ -17,22 +17,6 @@ class JsonObjectTests(unittest.TestCase):
             print(self.obj["wtf"])
         
         self.assertTrue("This key doesn't exist. Use get method to give a default value." in str(ctx.exception))
-
-    def test_to_xml(self):
-        self.assertEqual(self.obj.to_xml(), '<?xml version="1.0" encoding="UTF-8"?>\n<root>\n   <config>other</config>\n</root>')
-        o2 = json.JsonObject("doesnt_be_saved.json", { 
-            "config": "other",
-            "joueurs": {
-                "michel": 3
-            },
-            "score": [
-                {
-                    "oui": "non"
-                },
-                "non"
-            ]
-        }, saved_destroy=False)
-        self.assertEqual(o2.to_xml(), '<?xml version="1.0" encoding="UTF-8"?>\n<root>\n   <config>other</config>\n   <joueurs>\n      <michel>3</michel>\n   </joueurs>\n   <score>\n      <element>\n         <oui>non</oui>\n      </element>\n      <element>non</element>\n   </score>\n</root>')
     
     def test_clear(self):
         obj = json.JsonObject("osef.json", {"test": "oui"}, saved_destroy=False)
